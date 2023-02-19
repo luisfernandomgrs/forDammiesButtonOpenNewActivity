b775523340ef2d5fc6bfd59830c92ce682f8fc60
Luis Fernando Machado <>
2023-02-19 08:35:24 -0300

    Added an Up Button to the AppBar

--- app/src/main/java/com/codinginflow/buttonopenanewactivity/Activity2.java
+++ app/src/main/java/com/codinginflow/buttonopenanewactivity/Activity2.java
@@ -10,5 +10,7 @@ public class Activity2 extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_2);
+
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 }
